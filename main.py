4a6aae5a2ab87531214d16be3d95a0e493ef5f51
AlexandroJoeClaudio <91466410+AlexandroJoeClaudio@users.noreply.github.com>
2022-01-17 03:06:54 +0700

    updated comments

--- main.py
+++ main.py
@@ -24,16 +24,16 @@ left_frame.place(x = 0, y = utils.percentage_height(25))
 center_frame = Frame(root, bg = "violet", width = utils.percentage_widht(75), height = utils.percentage_height(75))
 center_frame.place(x = utils.percentage_widht(25), y = utils.percentage_height(25))
 
-for x in range(8):
+for x in range(8): #loop to create the the 8x8 grid
     for y in range(8):
         c = Cell(x, y)
         c.create_button(center_frame)
         c.cell_button_object.grid(column = x, row = y)
 
-Cell.create_cell_title_label(top_frame)
-Cell.cell_title.place(x = utils.percentage_widht(25), y = 0)
-Cell.create_cell_counter_label(left_frame)
-Cell.cell_label.place(x = 0, y = 0)
+Cell.create_cell_title_label(top_frame) #to create title
+Cell.cell_title.place(x = utils.percentage_widht(25), y = 0) #title position
+Cell.create_cell_counter_label(left_frame) #to create cell counter label
+Cell.cell_label.place(x = 0, y = 0) #cell position
 Cell.random_mines()
 
 #debug line to make sure to make the mines
