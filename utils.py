57996be0df384c4f52f27df3a0f87b806d5bfd6f
AlexandroJoeClaudio <91466410+AlexandroJoeClaudio@users.noreply.github.com>
2022-01-17 03:08:39 +0700

    updated comments

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import setting
 
-def percentage_height(percentage):
+def percentage_height(percentage):# to set the percentage of the height
     return (setting.height / 100) * percentage
 
-def percentage_widht(percentage):
+def percentage_widht(percentage):# to set the percentage of the width
     return (setting.widht / 100) * percentage
