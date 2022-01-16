317154bfb0b3582b1d1e0c088868ab20313b9292
AlexandroJoeClaudio <91466410+AlexandroJoeClaudio@users.noreply.github.com>
2022-01-17 03:07:58 +0700

    updated comments

--- setting.py
+++ setting.py
@@ -1,5 +1,4 @@
-widht = 1003
-height = 759
-cell_count = 8 ** 2
-# gridSize = 
-mine_count = (8 ** 2) // 4
+widht = 1003 #width of the screen
+height = 759 #height of the screen
+cell_count = 8 ** 2 #cells on the game
+mine_count = (8 ** 2) // 4 #mines on the game
