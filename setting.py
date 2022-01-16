8292d5678143e99e92cc064ebb49438a1246b346
AlexandroJoeClaudio <91466410+AlexandroJoeClaudio@users.noreply.github.com>
2022-01-16 14:01:48 +0700

    Final commit

---
+++ setting.py
@@ -0,0 +1,5 @@
+widht = 1003
+height = 759
+cell_count = 8 ** 2
+# gridSize = 
+mine_count = (8 ** 2) // 4
---
+++ utils.py
@@ -0,0 +1,7 @@
+import setting
+
+def percentage_height(percentage):
+    return (setting.height / 100) * percentage
+
+def percentage_widht(percentage):
+    return (setting.widht / 100) * percentage
